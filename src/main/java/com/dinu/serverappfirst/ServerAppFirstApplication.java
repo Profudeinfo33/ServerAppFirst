f9e8a6e7f31aea83d72097eb104a246ffd5c64cd
Profudeinfo33 <dinu_petre26@yahoo.ro>
2021-11-11 22:58:45 +0200

    Implement db properties and create one test db

--- src/main/java/com/dinu/serverappfirst/ServerAppFirstApplication.java
+++ src/main/java/com/dinu/serverappfirst/ServerAppFirstApplication.java
@@ -1,7 +1,14 @@
 package com.dinu.serverappfirst;
 
+import com.dinu.serverappfirst.model.Server;
+import com.dinu.serverappfirst.repository.ServerRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+import static com.dinu.serverappfirst.enumeration.Status.SERVER_DOWN;
+import static com.dinu.serverappfirst.enumeration.Status.SERVER_UP;
 
 @SpringBootApplication
 public class ServerAppFirstApplication {
@@ -9,5 +16,15 @@ public class ServerAppFirstApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServerAppFirstApplication.class, args);
 	}
+
+	@Bean
+	CommandLineRunner run(ServerRepository serverRepository){
+		return args -> {
+			serverRepository.save(new Server(null, "192.168.1.160", "Ubuntu Linux", "16GB", "Personal PC", "http://localhost:8080/server/image/server1.png", SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.58", "Fedora Linux", "16GB", "Office PC", "http://localhost:8080/server/image/server2.png", SERVER_DOWN));
+			serverRepository.save(new Server(null, "192.168.1.21", "MS 2008", "32GB", "Home PC", "http://localhost:8080/server/image/server3.png", SERVER_UP));
+			serverRepository.save(new Server(null, "192.168.1.14", "RED Linux", "64GB", "Laptop", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
+		};
+		}
 
 }
