b1c7a684f66cfb2afba0ebec3ab7a960a9897422
Profudeinfo33 <dinu_petre26@yahoo.ro>
2021-11-11 03:42:32 +0200

    Initial commit

---
+++ src/main/java/com/dinu/serverappfirst/ServerAppFirstApplication.java
@@ -0,0 +1,13 @@
+package com.dinu.serverappfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServerAppFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServerAppFirstApplication.class, args);
+	}
+
+}
