e7081475e401a43e1c371c736cd71735f83b0033
dinu2 <dinu_petre26@yahoo.ro>
2022-01-22 01:52:51 +0200

    Origin

--- src/main/java/com/dinu/serverappfirst/ServerAppFirstApplication.java
+++ src/main/java/com/dinu/serverappfirst/ServerAppFirstApplication.java
@@ -2,10 +2,17 @@ package com.dinu.serverappfirst;
 
 import com.dinu.serverappfirst.model.Server;
 import com.dinu.serverappfirst.repository.ServerRepository;
+import org.apache.catalina.filters.CorsFilter;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.reactive.CorsWebFilter;
+import org.springframework.web.cors.reactive.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
 
 import static com.dinu.serverappfirst.enumeration.Status.SERVER_DOWN;
 import static com.dinu.serverappfirst.enumeration.Status.SERVER_UP;
@@ -26,5 +33,18 @@ public class ServerAppFirstApplication {
 			serverRepository.save(new Server(null, "192.168.1.14", "RED Linux", "64GB", "Laptop", "http://localhost:8080/server/image/server4.png", SERVER_DOWN));
 		};
 		}
-
-}
+	@Bean
+	public CorsWebFilter corsWebFilter() {
+		UrlBasedCorsConfigurationSource urlBasedCorsConfigurationSource = new UrlBasedCorsConfigurationSource();
+		CorsConfiguration corsConfiguration = new CorsConfiguration();
+		corsConfiguration.setAllowCredentials(true);
+		corsConfiguration.setAllowedOrigins(Arrays.asList("http://localhost:3000", "http://localhost:4200"));
+		corsConfiguration.setAllowedHeaders(Arrays.asList("Origin", "Access-Control-Allow-Origin", "Content-Type",
+				"Accept", "Jwt-Token", "Authorization", "Origin, Accept", "X-Requested-With",
+				"Access-Control-Request-Method", "Access-Control-Request-Headers"));
+		corsConfiguration.setExposedHeaders(Arrays.asList("Origin", "Content-Type", "Accept", "Jwt-Token", "Authorization",
+				"Access-Control-Allow-Origin", "Access-Control-Allow-Origin", "Access-Control-Allow-Credentials", "Filename"));
+		corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
+		urlBasedCorsConfigurationSource.registerCorsConfiguration("/**", corsConfiguration);
+		return new CorsWebFilter(urlBasedCorsConfigurationSource);
+	} }
